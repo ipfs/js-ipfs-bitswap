6558afee0d423980dfbbad6daa0fde9369f8d9e2
Richard Schneider <makaretu@gmail.com>
2017-11-08 16:11:03 +1300

    test: Math.Random => uuid
    Math.Random can create collisions.  Using uuid should not create a collision.

--- test/utils/make-block.js
+++ test/utils/make-block.js
@@ -4,9 +4,10 @@ const multihashing = require('multihashing-async')
 const CID = require('cids')
 const Block = require('ipfs-block')
 const Buffer = require('safe-buffer').Buffer
+const uuid = require('uuid/v4')
 
 module.exports = (callback) => {
-  const data = Buffer.from(`hello world ${Math.random()}`)
+  const data = Buffer.from(`hello world ${uuid()}`)
 
   multihashing(data, 'sha2-256', (err, hash) => {
     if (err) { return callback(err) }
