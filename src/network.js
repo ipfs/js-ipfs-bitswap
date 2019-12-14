ad0d29ca5d62b7467be75bf2c1fd8cd3a9a8234d
Jacob Heun <jacobheun@gmail.com>
2019-12-14 16:50:21 +0100

    fix: networking cleanup and add bufferlist slice

--- src/network.js
+++ src/network.js
@@ -16,21 +16,24 @@ class Network {
     options = options || {}
     this.libp2p = libp2p
     this.bitswap = bitswap
-    this.b100Only = options.b100Only || false
+    this.protocols = [BITSWAP100]
+    if (!options.b100Only) {
+      // Latest bitswap first
+      this.protocols.unshift(BITSWAP110)
+    }
 
     this._stats = stats
     this._running = false
+
+    // bind event listeners
+    this._onPeerConnect = this._onPeerConnect.bind(this)
+    this._onPeerDisconnect = this._onPeerDisconnect.bind(this)
+    this._onConnection = this._onConnection.bind(this)
   }
 
   start () {
     this._running = true
-    // bind event listeners
-    this._onPeerConnect = this._onPeerConnect.bind(this)
-    this._onPeerDisconnect = this._onPeerDisconnect.bind(this)
-
-    this._onConnection = this._onConnection.bind(this)
-    this.libp2p.handle(BITSWAP100, this._onConnection)
-    if (!this.b100Only) { this.libp2p.handle(BITSWAP110, this._onConnection) }
+    this.libp2p.handle(this.protocols, this._onConnection)
 
     this.libp2p.on('peer:connect', this._onPeerConnect)
     this.libp2p.on('peer:disconnect', this._onPeerDisconnect)
@@ -46,17 +49,25 @@ class Network {
   stop () {
     this._running = false
 
-    this.libp2p.unhandle(BITSWAP100)
-    if (!this.b100Only) { this.libp2p.unhandle(BITSWAP110) }
+    // Unhandle both, libp2p doesn't care if it's not already handled
+    this.libp2p.unhandle(this.protocols)
 
     this.libp2p.removeListener('peer:connect', this._onPeerConnect)
     this.libp2p.removeListener('peer:disconnect', this._onPeerDisconnect)
   }
 
-  // Handles both types of bitswap messgages
+  /**
+   * Handles both types of incoming bitswap messages
+   * @private
+   * @param {object} param0
+   * @param {string} param0.protocol The protocol the stream is running
+   * @param {Stream} param0.stream A duplex iterable stream
+   * @param {Connection} param0.connection A libp2p Connection
+   * @returns {void}
+   */
   _onConnection ({ protocol, stream, connection }) {
     if (!this._running) { return }
-    this._log('incomming new bitswap connection: %s', protocol)
+    this._log('incoming new bitswap %s connection from %s', protocol, connection.remotePeer.toB58String())
 
     pipe(
       stream,
@@ -64,7 +75,7 @@ class Network {
       async (source) => {
         for await (const data of source) {
           try {
-            const message = await Message.deserialize(data)
+            const message = await Message.deserialize(data.slice())
             this.bitswap._receiveMessage(connection.remotePeer, message)
           } catch (err) {
             this.bitswap._receiveError(err)
@@ -75,14 +86,10 @@ class Network {
   }
 
   _onPeerConnect (peerInfo) {
-    if (!this._running) { return }
-
     this.bitswap._onPeerConnected(peerInfo.id)
   }
 
   _onPeerDisconnect (peerInfo) {
-    if (!this._running) { return }
-
     this.bitswap._onPeerDisconnected(peerInfo.id)
   }
 
@@ -127,7 +134,7 @@ class Network {
   async sendMessage (peer, msg) {
     if (!this._running) throw new Error('network isn\'t running')
 
-    const stringId = peer.toB58String() ? peer.toB58String() : peer.id.toB58String()
+    const stringId = peer.toB58String()
     this._log('sendMessage to %s', stringId, msg)
 
     const { stream, protocol } = await this._dialPeer(peer)
