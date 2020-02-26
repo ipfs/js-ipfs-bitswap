b41a8257b5e590e80557991502d8edae074f79e6
Dirk McCormick <dirkmdev@gmail.com>
2020-02-26 15:48:30 -0500

    docs: add some docs to task merger

--- src/decision-engine/task-merger.js
+++ src/decision-engine/task-merger.js
@@ -42,11 +42,66 @@ const TaskMerger = {
    * @param {Task} existingTask
    */
   merge (newTask, existingTask) {
+    // Tasks look like this:
+    // {
+    //   topic: "some topic",
+    //   priority: 5,
+    //
+    //   # The size of the response on the wire. This is used to calculate
+    //   # how many tasks to pop off the request queue and add to a message.
+    //   # If the response is
+    //   # - a HAVE or DONT_HAVE
+    //   #   it is the size of the CID + type (HAVE/DONT_HAVE)
+    //   # - a block
+    //   #   it is the size of the block
+    //   size: 32,
+    //
+    //   data: {
+    //
+    //     # The size of the block, if known (if we don't have the block this is zero)
+    //     blockSize: 128 * 1024,
+    //
+    //     # Indicates if the request is for a block or for a HAVE
+    //     isWantBlock: false,
+    //
+    //     # Do we have the block?
+    //     # Note: a block can have size zero.
+    //     haveBlock: true,
+    //
+    //     # Indicates whether to send a DONT_HAVE response if we don't have
+    //     # the block.
+    //     # If this is false and we don't have the block, we just ignore the
+    //     # want-block request (useful for discovery where we query lots of
+    //     # peers but don't want a response unless the peer has the block).
+    //     sendDontHave: false
+    //   }
+    // }
+    //
+    // The merge function ignores the topic and priority as these don't change.
+    //
+    // We may receive new information about a want before the want has been
+    // popped from the queue in the following scenarios:
+    //
+    // - Replace want type:
+    //   1. Client sends want-have CID1
+    //   2. Client sends want-block CID1
+    //   In this case we should replace want-have with want-block, including
+    //   updating the task size to be the block size.
+    //
+    // - Replace DONT_HAVE with want:
+    //   1. Client sends want-have CID1 or want-block CID1
+    //   2. Local node doesn't have block for CID1
+    //   3. Local node receives block for CID1 from peer
+    //   In this case we should replace DONT_HAVE with the want, including
+    //   updating the task size and block size.
+    //
+
     const taskData = newTask.data
     const existingData = existingTask.data
 
-    // If we now have block size information, update the task with
-    // the new block size
+    // If we didn't have block size information (because we didn't have the
+    // block) and we receive the block from a peer, update the task with the
+    // new block size
     if (!existingData.haveBlock && taskData.haveBlock) {
       existingData.haveBlock = taskData.haveBlock
       existingData.blockSize = taskData.blockSize
