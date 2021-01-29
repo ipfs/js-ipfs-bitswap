bbb2577bbc4963a5a6659b51a3400d483fc78cf0
Alex Potsides <alex@achingbrain.net>
2021-01-29 10:49:43 +0000

    chore: update deps (#291)
    Also removes repo fixture as the repo can init itself.

--- test/utils/create-temp-repo-nodejs.js
+++ test/utils/create-temp-repo-nodejs.js
@@ -3,18 +3,12 @@
 const IPFSRepo = require('ipfs-repo')
 const pathJoin = require('path').join
 const os = require('os')
-const ncp = require('ncp')
 const rimraf = require('rimraf')
 const promisify = require('promisify-es6')
-
-const baseRepo = pathJoin(__dirname, '../fixtures/repo')
 
 async function createTempRepo () {
   const date = Date.now().toString()
   const path = pathJoin(os.tmpdir(), `bitswap-tests-${date}-${Math.random()}`)
-
-  await promisify(ncp)(baseRepo, path)
-
   const repo = new IPFSRepo(path)
 
   repo.teardown = async () => {
@@ -22,6 +16,7 @@ async function createTempRepo () {
     await promisify(rimraf)(path)
   }
 
+  await repo.init({})
   await repo.open()
 
   return repo
